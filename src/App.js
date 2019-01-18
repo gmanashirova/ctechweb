b3dac25f586b16591dfc748bbfd1ed3f243a7736
gmanashirova <gmanashirova@hotmail.com>
2019-01-17 22:51:17 -0800

    new app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+       
+            Learn React
+      </div>
+    );
+  }
+}
+
+export default App;
